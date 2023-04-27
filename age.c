346a799e2dda804a7bd87c8bc301513ae495d20c
ALOUYA MOUT <110349578+ALOYA-MOUT@users.noreply.github.com>
2023-04-27 23:08:34 +0000

    Add files via upload
    input 2 integers and outputs them within a string

---
+++ age.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int h ;
+    int b ;
+    int x ;
+    int j ;
+    printf("what's your age ?");
+    scanf("%i",&h);
+    printf("how about your hight and wight");
+    scanf("%i and %i", &b , &x);
+    printf(" Your all good");
+    printf("your payment is %i " , b );
+}
